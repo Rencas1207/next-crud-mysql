e539d1b185ebe45853b5cae349e9688387ee1a71
Rencas <rencasdag.12@gmail.com>
2024-01-22 17:56:23 -0500

    database connection

---
+++ src/app/about/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-2xl font-bold my-4">About</h1>
+      <p className="text-gray-300">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
+        magnam sint nesciunt quos adipisci molestias blanditiis. Quia in dicta
+        sed quas sint magni odit vitae libero harum ut, recusandae nulla
+        deserunt voluptates exercitationem earum itaque iste quaerat nam
+        repellat rem porro voluptate dolor similique. Eveniet tempora fugit
+        molestias? Ducimus, omnis!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/api/products/route.js
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+
+export function GET() {
+   return NextResponse.json('listando productos')
+}
+
+export function POST() {
+   return NextResponse.json('creando productos')
+
+}
